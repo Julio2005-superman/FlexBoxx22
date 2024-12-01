2ace3e2c4b3aea74cba2b9ecad01d34bfbc272c0
Julio2005-superman <julioramirez0525@gmail.com>
2024-11-30 19:17:33 -0600

    Add files via upload

---
+++ js/operadores.js
@@ -0,0 +1,13 @@
+let i=8, j=3;
+let z=1;
+console.log('z=${z}');
+z=1;
+z=z++;
+console.log('z=${z}');
+z=1;
+z=++z;
+console.log('z=${z}');
+let y=2;
+y=(y++)+(++y);
+y=2;
+y=(++y)-(++y)+(y++);
---
+++ js/tipos.js
@@ -0,0 +1,9 @@
+//Declaración de variable en sus tipos de datos
+//anteriormente
+var entero = 10; //variable global
+//Nueva forma de declarar variables
+let radio = 10.0;
+let cadena = "Programación JavaScript";
+let centinela = false;
+let letra = 'A';
+const PI = 3.1416; //una constante
